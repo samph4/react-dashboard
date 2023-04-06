1090a9320d38020d2a8c897a735ceda80fcc32c0
Sam <smarkthompson@outlook.com>
2023-04-06 21:40:09 +0100

    fix: fix error in theme.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,20 @@
+import { ColorModeContext, useMode } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+
 function App() {
+  const [theme, colorMode] = useMode();
+
   return (
-    <div className="app">
-     <h1>Test</h1>
-    </div>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <main className="content">
+
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 
